a4b5904c076678e52a09b0dc3b03f07d4ab33c4b
”Leonid <surpraiz50@gmail.com>
2023-07-03 14:04:05 +0300

    7,2,5

--- src/test/java/exercise/article/WorkerImplTest.java
+++ src/test/java/exercise/article/WorkerImplTest.java
@@ -1,63 +1,121 @@
 package exercise.article;
 
-import org.junit.jupiter.api.Assertions;
+import exercise.worker.WorkerImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-public class WorkerImplTest {
+class WorkerImplTest {
 
+    private WorkerImpl worker;
+
+    @Mock
     private Library library;
 
     @BeforeEach
-    public void setup() {
-        library = new LibraryImpl();
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        worker = new WorkerImpl(library);
     }
 
     @Test
-    public void testGetAllTitles() {
-        List<String> expectedTitles = new ArrayList<>();
+    public void AddNewArticle() {
+        List<Article> art = new ArrayList<>();
 
-        expectedTitles.add("Сколько времени нужно, чтобы выучить Java");
-        expectedTitles.add("7 мифов об IT");
+        art.add(new Article(
+                "Новая тема",
+                "Новое описание статьи",
+                "Новый автор",
+                LocalDate.of(2023, 6, 13)));
 
-        List<String> actualTitles = library.getAllTitles();
-
-        Assertions.assertEquals(expectedTitles, actualTitles);
+        worker.addNewArticles(art);
+        verify(library).store(2023, art);
     }
 
     @Test
-    public void testStoreAndUpdateCatalog() {
-        List<Article> newArticles = new ArrayList<>();
-        newArticles.add(new Article(
-                "Новая статья 1",
-                "Содержимое новой статьи 1",
-                "Автор 1",
-                LocalDate.of(2023, 6, 14)));
-        newArticles.add(new Article(
-                "Новая статья 2",
-                "Содержимое новой статьи 2",
-                "Автор 2",
-                LocalDate.of(2023, 6, 14)));
-
-        library.store(2023, newArticles);
-        library.updateCatalog();
-
-        List<String> expectedTitles = new ArrayList<>();
-        expectedTitles.add("Как правильно изучать языки программирования");
-        expectedTitles.add("Как составить резюме тестировщику");
-        expectedTitles.add("Почему важны soft skills?");
-        expectedTitles.add("Сколько времени нужно, чтобы выучить Java");
-        expectedTitles.add("7 мифов об IT");
-        expectedTitles.add("Новая статья 1");
-        expectedTitles.add("Новая статья 2");
-
-        List<String> actualTitles = library.getAllTitles();
-
-        Assertions.assertEquals(expectedTitles, actualTitles);
+    public void getCatalogEmpty() {
+        when(library.getAllTitles()).thenReturn(new ArrayList<>());
+        String catalog = worker.getCatalog();
+        assertFalse(catalog.isEmpty());
     }
 
+    @Test
+    public void getCatalogWithArticles() {
+        List<String> titles = List.of("Статья 1", "Статья 2", "Статья 3");
+        when(library.getAllTitles()).thenReturn(titles);
+        String catalog = worker.getCatalog();
+        for (String title : titles) {
+            assertTrue(catalog.contains(title));
+        }
+    }
+
+    @Test
+    public void prepareArticlesRemovesArticlesWithoutTitle() {
+        List<Article> art = new ArrayList<>();
+        art.add(new Article(null, "Содержимое 1", "Автор 1", LocalDate.of(2023, 6, 13)));
+        art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 6, 14)));
+
+        List<Article> preparedArticles = worker.prepareArticles(art);
+        assertEquals(1, preparedArticles.size());
+        assertEquals("Статья 2", preparedArticles.get(0).getTitle());
+    }
+
+    @Test
+    public void prepareArticlesRemovesArticlesWithoutContent() {
+        List<Article> art = new ArrayList<>();
+        art.add(new Article("Статья 1", null, "Автор 1", LocalDate.of(2023, 6, 13)));
+        art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 6, 14)));
+
+        List<Article> preparedArticles = worker.prepareArticles(art);
+        assertEquals(1, preparedArticles.size());
+        assertEquals("Статья 2", preparedArticles.get(0).getTitle());
+    }
+
+    @Test
+    public void prepareArticlesSetsDefaultAuthor() {
+        List<Article> art = new ArrayList<>();
+        art.add(new Article("Статья 1", "Содержимое 1", null, LocalDate.of(2023, 6, 13)));
+        art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 6, 14)));
+
+        List<Article> preparedArticles = worker.prepareArticles(art);
+        assertEquals(1, preparedArticles.size());
+        assertEquals("Автор 2", preparedArticles.get(0).getAuthor());
+    }
+
+    @Test
+    public void prepareArticlesSetsDefaultCreationDate() {
+        List<Article> art = new ArrayList<>();
+        art.add(new Article("Статья 1", "Содержимое 1", "Автор 1", LocalDate.of(2023, 6, 13)));
+        art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", null));
+
+        List<Article> preparedArticles = worker.prepareArticles(art);
+        assertEquals(LocalDate.now(), preparedArticles.get(1).getCreationDate());
+    }
+
+    @Test
+    public void prepareArticlesRemovesArticlesWithSameTitleAndDifferentCreationDate() {
+        List<Article> art = new ArrayList<>();
+        art.add(new Article("Статья 1", "Содержимое 1", "Автор 1", LocalDate.of(2023, 6, 13)));
+        art.add(new Article("Статья 1", "Содержимое 2", "Автор 2", null));
+
+        List<Article> preparedArticles = worker.prepareArticles(art);
+        assertEquals(2, preparedArticles.size());
+    }
+
+    @Test
+    public void addNewArticlesCallsLibraryStore() {
+        List<Article> art = new ArrayList<>();
+        art.add(new Article("Статья 1", "Содержимое 1", "Автор 1", LocalDate.of(2023, 6, 13)));
+        art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 6, 14)));
+
+        worker.addNewArticles(art);
+        verify(library).store(2023, art);
+    }
 }
